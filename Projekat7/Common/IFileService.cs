6fa804ee459249dda1cc91db78cadae7792e6538
Administrator <Administrator@P05-10>
2018-11-05 17:17:38 +0100

    common up

--- Projekat7/Common/IFileService.cs
+++ Projekat7/Common/IFileService.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common
 {
+    [ServiceContract]
    public interface IFileService
     {
+        [OperationContract]
+        void CreateFolder(string foldername);
     }
 }
