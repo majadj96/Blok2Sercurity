bdc92df77e06c8dce9a9a40bf1728d412d2e3d91
Administrator <Administrator@P05-10>
2018-11-06 16:46:53 +0100

    fje

--- Projekat7/Service/Program.cs
+++ Projekat7/Service/Program.cs
@@ -13,35 +13,39 @@ namespace Service
 {
     class Program
     {
-        static void Main(string[] args)
+        static ServiceHost CreateHostForRBAC()
         {
-
             string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
             NetTcpBinding binding = new NetTcpBinding();
             binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
             string address = "net.tcp://localhost:50001/UpdateConfig";
             ServiceHost hostForRBAC = new ServiceHost(typeof(UpdateConfig));
             hostForRBAC.AddServiceEndpoint(typeof(IUpdateConfig), binding, address);
-
             hostForRBAC.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
             hostForRBAC.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
-
             hostForRBAC.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
-
             hostForRBAC.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
 
             try
             {
                 hostForRBAC.Open();
                 Console.WriteLine("Service for RBAC host is started.");
+                return hostForRBAC;
             }
             catch (Exception e)
             {
                 Console.WriteLine("[ERROR] {0}", e.Message);
-              //  Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+                //  Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+                return null;
             }
-          
+        }
 
+
+        static void Main(string[] args)
+        {
+            
+            ServiceHost hostForRBAC = CreateHostForRBAC();
+          
             Console.WriteLine("Choose 't' for Transport Mode or 'm' for Message Mode");
             string mode = Console.ReadLine();
 
@@ -56,5 +60,7 @@ namespace Service
 
 
         }
+
+      
     }
 }
