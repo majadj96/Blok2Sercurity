69cd5c4f97ff8993b226bb7d538c0f38df922c03
Administrator <Administrator@P05-10>
2018-11-06 16:16:01 +0100

    update configuration

--- Projekat7/Service/Program.cs
+++ Projekat7/Service/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +15,12 @@ namespace Service
         {
 
             string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            string address = "net.tcp://localhost:9999/Receiver";
+            ServiceHost host = new ServiceHost(typeof(UpdateConfig));
+            host.AddServiceEndpoint(typeof(IUpdateConfig), binding, address);
+
 
 
 
