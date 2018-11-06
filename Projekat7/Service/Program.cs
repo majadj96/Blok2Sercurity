778d386dc8ef64d86bb2116c4488f86c2b485aa3
Administrator <Administrator@P05-10>
2018-11-06 16:21:05 +0100

    svasta nesahtowwww

--- Projekat7/Service/Program.cs
+++ Projekat7/Service/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
 using System.ServiceModel;
+using System.ServiceModel.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,9 +18,12 @@ namespace Service
             string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
             NetTcpBinding binding = new NetTcpBinding();
             binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
-            string address = "net.tcp://localhost:9999/Receiver";
+            string address = "net.tcp://localhost:9999/UpdateConfig";
             ServiceHost host = new ServiceHost(typeof(UpdateConfig));
             host.AddServiceEndpoint(typeof(IUpdateConfig), binding, address);
+
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
 
 
 
