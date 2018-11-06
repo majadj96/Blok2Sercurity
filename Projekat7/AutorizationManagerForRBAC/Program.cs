f5a8edae2881359a0681c85b6c509a88bb01f117
elena1237 <elenakevac@gmail.com>
2018-11-06 16:48:58 +0100

    autorizationAsRBACClient

--- Projekat7/AutorizationManagerForRBAC/Program.cs
+++ Projekat7/AutorizationManagerForRBAC/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,16 @@ namespace AutorizationManagerForRBAC
     {
         static void Main(string[] args)
         {
-            
+
+            /// Define the expected service certificate. It is required to establish cmmunication using certificates.
+            string srvCertCN = "Servis";
+            NetTcpBinding binding1 = new NetTcpBinding();
+
             ServiceHost host;
             NetTcpBinding binding = new NetTcpBinding();
 
             string address = "net.tcp://localhost:9998/RBACChange";
+
 
             host = new ServiceHost(typeof(RBACChange));
             host.AddServiceEndpoint(typeof(IRBACChange), binding, address);
@@ -25,7 +31,24 @@ namespace AutorizationManagerForRBAC
 
             Console.WriteLine("I'm ready for changes 8-)");
 
-            Console.ReadLine();
+
+
+
+            binding1.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address1 = new EndpointAddress(new Uri("net.tcp://localhost:9999/Receiver"),
+                                      new X509CertificateEndpointIdentity(srvCert));
+
+            using (MakeRBACClient proxy = new MakeRBACClient(binding1, address1))
+            {
+
+                /// 1. Communication test
+				proxy.UpdateConfiguration();
+                Console.WriteLine("Update() finished. Press <enter> to continue ...");
+                Console.ReadLine();
+            }
+           
             host.Close();
 
 
