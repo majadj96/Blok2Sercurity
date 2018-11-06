b79ce7d37115c3aec12751605dc4807a5468f020
Administrator <Administrator@P05-10>
2018-11-06 14:43:05 +0100

    cao ely

--- Projekat7/AutorizationManagerForRBAC/Program.cs
+++ Projekat7/AutorizationManagerForRBAC/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,23 @@ namespace AutorizationManagerForRBAC
     {
         static void Main(string[] args)
         {
-            //RBAC
-            //dodoato
+            
+            ServiceHost host;
+            NetTcpBinding binding = new NetTcpBinding();
+
+            string address = "net.tcp://localhost:9999/RBACChange";
+
+            host = new ServiceHost(typeof(RBACChange));
+            host.AddServiceEndpoint(typeof(IRBACChange), binding, address);
+
+            host.Open();
+
+            Console.WriteLine("I'm ready for changes 8-)");
+
+            Console.ReadLine();
+            host.Close();
+
+
         }
     }
 }
---
+++ Projekat7/AutorizationManagerForRBAC/RBACChange.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutorizationManagerForRBAC
+{
+    class RBACChange : IRBACChange
+    {
+        public void Change()
+        {
+            Console.WriteLine("CAO ELENAAAAAAAAA");
+        }
+
+    }
+}
