20aeca9814d13471fd36f81da3c5555afe52f4c1
Administrator <Administrator@P05-24>
2018-11-09 15:04:49 +0100

    gotova metoda

--- Projekat7/AutorizationManagerForRBAC/Sync.cs
+++ Projekat7/AutorizationManagerForRBAC/Sync.cs
@@ -1,7 +1,9 @@
 ﻿using Common;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,19 @@ namespace AutorizationManagerForRBAC
     {
         public Dictionary<string, List<string>> SetDictionary()
         {
-            throw new NotImplementedException();
+            Dictionary<string, List<string>>  GroupsAndPermissionsDict = new Dictionary<string, List<string>>();
+
+            ResXResourceReader rsxr = new ResXResourceReader("..\\..\\GroupsAndPermisions.resx");
+            foreach (DictionaryEntry d in rsxr)
+            {
+
+                string name = d.Key.ToString();
+                string value = d.Value.ToString();
+                string[] split = value.Split(',');
+                List<string> listaPermisija = split.ToList();
+                GroupsAndPermissionsDict.Add(name, listaPermisija);
+            }
+            return GroupsAndPermissionsDict;
         }
     }
 }
